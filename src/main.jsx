4ca221b2571e06a3f11895372b4eb4450e81f1a7
gmzseverr <gmzsever12@gmail.com>
2025-01-16 17:43:21 +0300

    gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
